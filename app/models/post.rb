aff37a3702d71089d8282ba9eafa67fbfbef3ca5
Wesley Beary <monki@geemus.com>
2008-09-20 00:06:10 -0700

    working on adding files and a video/audio player

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,11 @@
+require 'dm-types'
+
 class Post
 
   include DataMapper::Resource
   property :id, String, { :key => true, :field => :_id, :nullable => true }
   property :rev, String, :field => :_rev
+  property :attachments, JsonObject, :field => :_attachments
   property :post_type, Discriminator
   property :created_at, DateTime
   property :updated_at, DateTime
