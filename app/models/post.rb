e5b3b9dfe7529a8f13763e0a526b4b5247954dfb
Wesley Beary <monki@geemus.com>
2008-09-20 16:02:06 -0700

    temporarily remove attachments as they are preventing records from saving properly

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,16 +5,18 @@ class Post
   include DataMapper::Resource
   property :id, String, { :key => true, :field => :_id, :nullable => true }
   property :rev, String, :field => :_rev
-  property :attachments, JsonObject, :field => :_attachments
+  # property :attachments, JsonObject, :field => :_attachments
   property :post_type, Discriminator
   property :created_at, DateTime
   property :updated_at, DateTime
 
   # FIXME: The name isn't getting persisted?
-  def file=(new_file)
-    self.name = new_file['filename']
-    @attachment = new_file
-    self.add_attachment(new_file['tempfile'], { :content_type => new_file['content_type'], :name => self.post_type.storage_name })
-  end
+  # def file=(new_file)
+  #   unless new_file.empty?
+  #     self.name = new_file['filename']
+  #     @attachment = new_file
+  #     self.add_attachment(new_file['tempfile'], { :content_type => new_file['content_type'], :name => self.post_type.storage_name })
+  #   end
+  # end
 
 end
