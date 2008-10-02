4444f69d3cc1f9585a15fc330a92aed2bfeb5e83
Wesley Beary <monki@geemus.com>
2008-10-01 19:13:21 -0700

    working on specs again/more/still

--- spec/controllers/posts_spec.rb
+++ spec/controllers/posts_spec.rb
@@ -1,7 +1,10 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
 
-describe Posts, "index action" do
-  before(:each) do
-    dispatch_to(Posts, :index)
+describe Posts, "index" do
+
+  it "should set posts variable" do
+    @controller = dispatch_to(Posts, :index)
+    @controller.instance_variable_get(:@posts).should == []
   end
+
 end
