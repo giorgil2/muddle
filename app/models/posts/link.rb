227c62e8e45109fb7c2cf82452b44c6ebbb02bc1
Wesley Beary <monki@geemus.com>
2008-12-08 21:44:24 -0800

    cleaning up link model/views

--- app/models/posts/link.rb
+++ app/models/posts/link.rb
@@ -1,7 +1,21 @@
+# TODO: tags
+# TODO: post out to del.icio.us
+
+require 'rubygems'
+require 'hpricot'
+require 'open-uri'
+
 class Link < Post
 
-  property :name, String
-  property :link, String, { :nullable => false }
-  property :description, Text, { :lazy => false }
+  property :url, String, { :nullable => false }
+  property :title, String, { :nullable => false }
+  property :notes, Text, { :lazy => false }
+
+  before(:valid?) do
+    unless @title && !@title.empty?
+      doc = Hpricot(open(url))
+      @title = doc.at('title').inner_html
+    end
+  end
 
 end
