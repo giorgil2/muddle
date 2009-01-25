3c7b65bab0c8611c5ba6db7f5930c2fdc55f9552
Wesley Beary <monki@geemus.com>
2009-01-25 15:40:07 -0800

    monkey patch around merb_datamapper repo blocks for now

--- config/init.rb
+++ config/init.rb
@@ -16,11 +16,20 @@ Merb::Config.use do |c|
   c[:session_secret_key]  = '2ba11c91acaa954ea631293e48e2c515be15b4c0'  # required for cookie session store
   # c[:session_id_key] = '_session_id' # cookie session id key, defaults to "_session_id"
 
-  Merb::Plugins.config[:merb_datamapper][:use_repository_block] = false
+  # TODO: use this setting instead of monkey-patch after point release.
+  # Merb::Plugins.config[:merb_datamapper][:use_repository_block] = false
 end
 
 Merb::BootLoader.after_app_loads do
   # Add dependencies here that must load after the application loads:
+
+  # FIXME: monkey-patch to prevent merb_datamapper repository blocks, remove after point release
+  class Application < Merb::Controller
+    override! :_call_action
+    def _call_action(*)
+      super
+    end
+  end
 end
 
 Merb.add_mime_type(:atom, :to_atom, %w[application/atom+xml])
