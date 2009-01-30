357c3939432b49eb92b508c4dddada0cce6e623d
Wesley Beary <monki@geemus.com>
2009-01-29 18:30:49 -0800

    fix location for analytics

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -1,9 +1,9 @@
 module Merb
   module GlobalHelpers
 
-    if File.exists?(File.join(Merb.root, '..', 'shared', 'analytics'))
+    if File.exists?(File.join(Merb.root, '..', '..', 'shared', 'analytics'))
       def analytics
-        File.read(File.join(Merb.root, '..', 'shared', 'analytics')).rstrip
+        File.read(File.join(Merb.root, '..', '..', 'shared', 'analytics')).rstrip
       end
     else
       def analytics
